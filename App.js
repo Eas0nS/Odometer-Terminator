547c9261cf52c08522cdc2a3472e636446430199
Ethan <shao44@wisc.edu>
2020-10-13 23:29:27 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+    state = {
+      email: '',
+      password: '',
+    };
+  
+  
+  onLogin() {
+    const { email, password } = this.state;
+
+    Alert.alert('Credentials', `email: ${email} + password: ${password}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.titleText}>Hi, Welcome To</Text>
+        <Text style={styles.titleText}>Momento</Text>
+        <TextInput
+          value={this.state.email}
+          keyboardType = 'email-address'
+          onChangeText={(email) => this.setState({ email })}
+          placeholder='email'
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'password'}
+          secureTextEntry={true}
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        
+     
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onLogin.bind(this)}
+       >
+         <Text style={styles.buttonText}> Sign Up / Login </Text>
+       </TouchableOpacity>
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'salmon',
+  },
+  titleText:{
+    fontFamily: 'Baskerville',
+    fontSize: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'powderblue',
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 25,
+    marginBottom: 10,
+  },
+  buttonText:{
+    fontFamily: 'Baskerville',
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    fontFamily: 'Baskerville',
+    fontSize: 20,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginVertical: 10,
   },
 });
