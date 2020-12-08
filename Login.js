6f96ae5608bb13dfeb57ea70471555ee7200d8f4
kunlunW <52982585+kunlunW@users.noreply.github.com>
2020-12-07 23:16:29 -0600

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, Image, View, Alert, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, Image, Button, View, Alert, TouchableOpacity, TextInput, Dimensions, } from 'react-native';
+
+// const { width, height } = Dimensions.get("window");
+
+// const background = require("./assets/first.png");
+// const mark = require("./assets/thrid.png");
+// const lockIcon = require("./assets/second.png");
+// const personIcon = require("./assets/fourth.png");
+
 
 class Login extends React.Component {
+
 
   constructor(props) {
     super(props)
@@ -10,7 +19,6 @@ class Login extends React.Component {
       password: "",
     }
   }
-    
   userLogin = () =>{
  
     const { username }  = this.state ;
@@ -42,48 +50,119 @@ class Login extends React.Component {
   
   render() {
     return (
+      
+      // <View style={styles.container}>
+      //   <Image 
+      //     style= {styles.backgroundImage}
+      //     source= {require('./assets/newBG3.png')}
+      //   />
+      //   {/* <Text style={styles.titleText}>Welcome to</Text>
+      //   <Text style={styles.titleText}>Odometer Terminator</Text> */}
+      //   <Image
+      //     style={{ width: "100%", height: 120, top:-110 }}
+      //     source={require('./assets/logo.png')}
+      //   />
+
+      //   <Text style={styles.logo}>Odometer Terminator</Text>
+
+
+      //   <TextInput
+      //     keyboardType = 'default'
+      //     onChangeText={(username) => this.setState({ username })}
+      //     placeholder='username'
+      //     placeholderTextColor = 'white'
+      //     style={styles.input}
+      //   />
+      //   <TextInput
+      //     onChangeText={(password) => this.setState({ password })}
+      //     placeholder={'password'}
+      //     secureTextEntry={true}
+      //     placeholderTextColor = 'white'
+      //     style={styles.input}
+      //   />
+      //   <TouchableOpacity
+      //     style={styles.button}
+      //     onPress={this.userLogin}
+      //   >
+      //     <Text style={styles.buttonText}> Login </Text>
+      //   </TouchableOpacity>
+      //   <TouchableOpacity
+      //     style={styles.button}
+      //     onPress={() =>
+      //        this.props.navigation.navigate('Register0')}
+      //   >
+      //     <Text style={styles.buttonText}> Register </Text>
+      //   </TouchableOpacity> 
+      // </View>
+
+
       <View style={styles.container}>
         <Image 
-          style= {styles.backgroundImage}
-          source= {require('./assets/bg2.png')}
-        />
-        <Text style={styles.titleText}>Welcome to</Text>
-        <Text style={styles.titleText}>Odometer Terminator</Text>
+           style= {styles.backgroundImage}
+           source= {require('./assets/newBG3.png')}
+         />
+
         <Image
-          style={{ width: 200, height: 100}}
-          source={require('./assets/logo.png')}
-        />
-        <TextInput
-          keyboardType = 'default'
-          onChangeText={(username) => this.setState({ username })}
-          placeholder='username'
-          placeholderTextColor = 'white'
-          style={styles.input}
-        />
-        <TextInput
-          onChangeText={(password) => this.setState({ password })}
-          placeholder={'password'}
-          secureTextEntry={true}
-          placeholderTextColor = 'white'
-          style={styles.input}
-        />
-        <TouchableOpacity
-          style={styles.button}
-          onPress={this.userLogin}
-        >
-          <Text style={styles.buttonText}> Login </Text>
+           style={{ width: "100%", height: 190, top:-110 }}
+           source={require('./assets/OD.png')}
+         />
+
+        <Text style={styles.logo}>Odometer Terminator</Text>
+
+
+        <View style={styles.inputContainer}>
+          <Image style={[styles.icon, styles.inputIcon]} source={{uri: "https://img.icons8.com/ios-glyphs/30/000000/name.png"}}/>
+          <TextInput style={styles.inputs}
+              keyboardType = 'default'
+              onChangeText={(username) => this.setState({ username })}
+
+              placeholder="username"
+              // keyboardType="email-address"
+              underlineColorAndroid='transparent'/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={[styles.icon, styles.inputIcon]} source={{uri: 'https://img.icons8.com/ios-glyphs/30/000000/forgot-password.png'}}/>
+          <TextInput style={styles.inputs}
+              onChangeText={(password) => this.setState({ password })}
+              placeholder="password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'/>
+        </View>
+     
+
+        <TouchableOpacity onPress={this.userLogin} style={[styles.buttonContainer, styles.loginButton]}>
+          <Text style={styles.loginText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-             this.props.navigation.navigate('Register0')}
-        >
-          <Text style={styles.buttonText}> Register </Text>
-        </TouchableOpacity> 
+
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('Register0')} style={[styles.buttonContainer, styles.loginButton]}>
+          <Text style={styles.loginText}>Register</Text>
+        </TouchableOpacity>
+        
+
+        {/* <TouchableOpacity style={[styles.buttonContainer, styles.fabookButton]}>
+          <View style={styles.socialButtonContent}>
+            <Image style={styles.icon} source={{uri: 'https://png.icons8.com/facebook/androidL/40/FFFFFF'}}/>
+            <Text style={styles.loginText}>Continue with facebook</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.buttonContainer, styles.googleButton]}>
+          <View style={styles.socialButtonContent}>
+            <Image style={styles.icon} source={{uri: 'https://png.icons8.com/google/androidL/40/FFFFFF'}}/>
+            <Text style={styles.loginText}>Sign in with google</Text>
+          </View>
+        </TouchableOpacity> */}
       </View>
+
+
+      
+      
+
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   backgroundImage:{
@@ -92,16 +171,18 @@ const styles = StyleSheet.create({
     left: 0,
     bottom: 0,
     right: 0,
-    opacity: 0.8
+    opacity: 0.8,
+    width:"100%",
+    height:"100%"
   },
   container: {
     flex: 1,
-    fontFamily: 'Courier',
+    fontFamily: 'Arial',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titleText:{
-    fontFamily: 'Courier',
+    fontFamily: 'Arial',
     fontWeight: 'bold',
     fontSize: 20,
     alignItems: 'center',
@@ -119,21 +200,145 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   buttonText:{
-    fontFamily: 'Courier',
+    fontFamily: 'Arial',
     fontSize: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
   input: {
     width: 200,
-    fontFamily: 'Courier',
+    fontFamily: 'Arial',
     fontSize: 20,
     height: 44,
     padding: 10,
     borderWidth: 1,
     borderColor: 'white',
-    marginBottom: 15
+    marginBottom:10,
+    top: -60,
+    backgroundColor: "gray",
+    opacity:0.9
   },
-});
+  /// 
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+
+  input2: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+
+  logo:{
+    fontFamily: 'Arial',
+    fontWeight:"bold",
+    fontSize:20,
+    color:"white",
+    // marginBottom:20
+    top: -100,
+  },
+
+
+
+
+
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#B0E0E6',
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:15,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  icon:{
+    width:30,
+    height:30,
+  },
+  inputIcon:{
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  loginButton: {
+    backgroundColor: '#3498db',
+  },
+  fabookButton: {
+    backgroundColor: "#3b5998",
+  },
+  googleButton: {
+    backgroundColor: "#ff0000",
+  },
+  loginText: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  restoreButtonContainer:{
+    width:250,
+    marginBottom:15,
+    alignItems: 'flex-end'
+  },
+  socialButtonContent:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center', 
+  },
+  socialIcon:{
+    color: "#FFFFFF",
+    marginRight:5
+  }, 
+  
+
+  button: {
+    display: 'flex',
+    height: 60,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#2AC062',
+    shadowColor: '#2AC062',
+    shadowOpacity: 0.5,
+    shadowOffset: { 
+      height: 10, 
+      width: 0 
+    },
+    shadowRadius: 25,
+  },
+    
+  });
 
 export default Login;
